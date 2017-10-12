09f1caf3c71b1bca1e237e1a4ad0e85d93124fb5
Eric Lin <eric@axiomzen.co>
2017-10-12 14:58:03 -0700

    gotta use process.env.PORT

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use('/', express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on PORT: ${process.env.PORT}`);
 });
